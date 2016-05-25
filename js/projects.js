afc88a8f3c4fa7731e7f15336bf85b1db868ba39
Karo Antonio <karoantonio@inside-166-60.wireless.utoronto.ca>
2016-05-24 21:38:52 -0400

    dynamicize

---
+++ js/projects.js
@@ -0,0 +1,9 @@
+//central location to pull projects from
+// button name , repo
+var projects = {
+	'wrote':'wrote',
+	'plan _':'the-plan',
+	'PANTONE':'pantone-selector',
+}
+
+
--- script.js
+++ script.js
@@ -1,36 +1,22 @@
 var selected_project = null;
 var selected_img = null;
 var og_root = null;
-
-var choice_tree = {
-    'KARO':['MADE','ON'],
-    'MADE':['THIS','PRETTY SOUNDS','POETRY','WRDCVLT','WHY STAY','TALK2ME'],
-    'ON':['GITHUB','TUMBLR'],
-}
+var table;
 
 $(document).ready(
     function () {
+		// ADD Projects
+		table = $('#side-bar ul');
+		Object.keys(projects).forEach(function(e, i , arr) {
+			build_project(e, projects[e]);
+		});
+
         //SET splash
 		var splash = '#beige'
         selected_project = $(splash);
         selected_project.addClass('splash');
         $('.project').css('display','none')
         toggle_display(splash)
-		/*
-
-        //TWEAK overlay:hover
-        $('#info').hover(function(event) {
-            $('.overlay').removeClass('hover-listener');
-        })
-        $('#root').hover(function() {
-            $('.overlay').addClass('hover-listener');
-        },function() {
-            $('.overlay').removeClass('hover-listener');
-        })
-
-        //STYLE overlay tree
-        style_overlay()
-		*/
 
         //Image toggle
         $('.cropped').click( function() {
@@ -47,6 +33,30 @@ $(document).ready(
             selected_img = $(this)
         })
     })
+
+function build_project( label, repo ) {
+	// label : label to be used on the bottun
+	// repo : the corresponding repo to sources
+	
+	// Add Project
+	var repo_root = "http://karoantonio.github.io/"
+	var proj = $('<iframe>');
+	proj.addClass('project');
+	proj.attr('id',repo);
+	proj.attr('data-src', repo_root + repo + "/");
+	$('#content').prepend(proj);
+	
+	// Add Button
+	var button = $('<div>');
+	var row = $('<li>');
+	button.html(label);
+	button.attr('id',repo + '_button');
+	row.prepend(button);
+	table.prepend(row);
+	button.click(function(){
+		toggle_display('#'+repo);
+	});
+}
 
 function send_email() {
     $.ajax({
@@ -72,61 +82,6 @@ function send_email() {
         console.log(response)
         $('#email_feedback')[0].innerHTML = response[0]['status'] + "!"
  });
-}
-
-
-function style_overlay() {
-    //Poorly designed
-    //TODO restructure as a table of trees
-    var karo = $('#root').children()[0]
-    //ADD Faux names
-    var num_faux = $('#root>li>ul>li').length
-            + $('#root>li>ul>li').last().children().last().children().length
-            - 2
-    for (i = 0; i < num_faux; i++) {
-        var new_karo = document.createElement("div");
-        new_karo.innerHTML = "KARO"
-        new_karo.id = "name_"+(i+1)
-        new_karo.style.display = 'none';
-        new_karo.className = 'name faux'
-        $('#root>li').first().append(new_karo)
-    }
-
-    //Try to display a menu of choices on hover
-    /*
-    $('#root>li:nth-child(2)').hover(function() {
-        $('#root>li>ul').css('display','block')
-        $('#choice_2').css('display','hidden')
-        $(this).css('display','hidden')
-    }, function() {
-        $('.sub-menu').css('display','hidden')
-        $('#root>li>ul').css('display','hidden')
-        $('#choice_2').css('display','block')
-        $('#root>li').css('display','inline-block')
-    })
-    */
-
-    $('#root>li>ul>li').hover(function() {
-        //DISPLAY all faux name elements
-        $('.faux').css('display','block')
-        //HIDE all names
-        $('.name').css('visibility','hidden')
-        //SHOW corresponding name
-        $('#name_'+$(this).index()).css('visibility','visible')
-    }, function() {
-        $('.faux').css('display','none')
-        $('.rootname').css('visibility','visible')
-    })
-
-    $('#root>li>ul>li>ul>li').hover(function() {
-        //HIDE all aunts and uncles
-        $('#root>li>ul>li').css('visibility','hidden')
-        //SHOW parent()
-        $(this).parent().parent().css('visibility','visible')
-    }, function() {
-        //SHOW all aunts and uncles
-        $('#root>li>ul>li').css('visibility','visible')
-    })
 }
 
 function toggle_display(id) {
