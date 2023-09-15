1dca2a47845446c0897a589b2a588f492aa9da52
karoantonio <karoantonio@gmail.com>
2023-09-15 11:51:03 -0700

    init

--- publish_to_gh_pages.sh
+++ publish_to_gh_pages.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
 # push master to gh-pages which in effect publishes the site
 
-git checkout --orphan gh-pages
+# Check if the gh-pages branch exists
+if git show-ref --verify --quiet "refs/heads/gh-pages"; then
+  # If the branch exists, switch to it
+  git checkout gh-pages
+else
+  # If the branch doesn't exist, create it as an orphan branch
+  git checkout --orphan gh-pages
+fi
+
+# Add all files (including untracked) to the staging area
 git add --all
+
+# Commit the changes
 git commit -m 'init'
+
+# Push the changes to the remote gh-pages branch with force
 git push -f origin gh-pages
+
+# Switch back to the master branch
 git checkout master
+
